24cf52ed6808ba53975a86d9055b5ede8c0e82a8
GiovanniConti <giovanni.conti@live.it>
2021-12-02 19:42:54 +0100

    Moved variable moviesList in a separate file

---
+++ data/moviesList.php
@@ -0,0 +1,15 @@
+<?php
+$moviesList = [
+  [
+    "title" => "The Godfather",
+    "releaseDate" => "1972",
+    "director" => "Francis Ford Coppola",
+    "genre" => "Crime, Drama",
+  ],
+  [
+    "title" => "The Irishman",
+    "releaseDate" => "2019",
+    "director" => "Martin Scorsese",
+    "genre" => "Biography, Crime, Drama",
+  ],
+];
