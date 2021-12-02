ef7690216f504016a201664ac851a8372778def0
GiovanniConti <giovanni.conti@live.it>
2021-12-02 19:44:33 +0100

    Foreach version

--- index.php
+++ index.php
@@ -5,12 +5,10 @@ require_once "./data/moviesList.php";
 // Version 1
 // $movie1 = new Movie($moviesList[0]);
 // $movie2 = new Movie($moviesList[1]);
-
 // var_dump($movie1);
 // var_dump($movie2);
 
 // Version using a foreach
-
 foreach ($moviesList as $movie) {
   $selectedMovie = new Movie($movie);
   var_dump($selectedMovie);
